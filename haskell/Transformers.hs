9918ba3f78ef1a8ad9350f0da75eca62f822b714
Daniel Brice <danielbrice@gmail.com>
2018-04-16 20:05:29 -0700

    delete all the implementations

--- haskell/Transformers.hs
+++ haskell/Transformers.hs
@@ -3,35 +3,21 @@
 module Transformers where
 
 import Spec
-import Control.Monad.Except (MonadError, throwError, runExceptT)
+import Control.Monad.Except (ExceptT, MonadError, throwError, runExceptT)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 
 implies :: MonadError e m => Bool -> e -> m ()
-failure `implies` fallback = if failure then throwError fallback else return ()
+failure `implies` fallback = undefined
 
 getUser :: MonadError Response m => Request -> m User
-getUser (Request _ _ _ header) = do
-  token <- maybe (throwError noToken) return $ lookup "Authorization" header
-  is_malformed_token `implies` malformedToken token
-  is_user_not_found `implies` noUser token
-  return the_user
+getUser (Request _ _ _ header) = undefined
 
 getResource :: (MonadError Response m, MonadIO m) => Request -> m Resource
-getResource (Request path _ _ _) = do
-  liftIO is_resource_not_found_io >>= (`implies` noResource path)
-  liftIO the_resource_io
+getResource (Request path _ _ _) = undefined
 
 execute :: (MonadError Response m, MonadIO m)
         => String -> User -> Resource -> m ()
-execute body usr (Resource path) = do
-  liftIO (not <$> is_permitted_io) >>= (`implies` notPermitted path)
-  liftIO (not <$> is_executed_io) >>= (`implies` badConnection)
+execute body usr (Resource path) = undefined
 
 handlePost :: Request -> IO Response
-handlePost req = (either id id <$>) . runExceptT $ do
-  (method req /= "POST") `implies` notAllowed (method req)
-  (null $ body req) `implies` noBody
-  usr <- getUser req
-  src <- getResource req
-  execute (body req) usr src
-  return $ success (path req) (body req)
+handlePost req = undefined
